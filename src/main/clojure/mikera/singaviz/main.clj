6725f30d8f51c1174fc267cd207c059bcba122af
mikera <mike.r.anderson.13@gmail.com>
2013-08-31 12:15:30 +0800

    implement data loading

--- src/main/clojure/mikera/singaviz/main.clj
+++ src/main/clojure/mikera/singaviz/main.clj
@@ -1,6 +1,11 @@
 (ns mikera.singaviz.main
   (:use [clojure.core.matrix])
+  (:require [clojure.edn :as edn])
   (:require [mikera.image.core :as img])
   (:require [clojure.java.io :as io]))
 
-(defonce data (read (io/reader "viz.data")))
+;; load the visualization data from the "data.viz" resource file
+(defonce data 
+  (mapv 
+    #(coerce :vectorz %) ;; convert each frame to a vectorz matrix
+    (with-open [in (java.io.PushbackReader. (io/reader (io/resource "viz.data")))] (edn/read in))))
